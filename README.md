5377f2b57fc561f04699e5732c2e59ec529ecc49
Angel Blanco <angelbt91@gmail.com>
2022-07-19 18:04:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Image Grid Uploader
+
+## Installation
+
+TODO
+
+## How to use
+
+TODO
---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SayHello from "react-image-grid-uploader";
+
+function App() {
+  return (
+    <div>
+        <SayHello name={"Angel"} />
+    </div>
+  );
+}
+
+export default App;
