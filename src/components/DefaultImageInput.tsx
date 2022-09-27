e3b63d751288ca2243e9958717bbd3b1c05bce8e
Angel Blanco <angelbt91@gmail.com>
2022-09-27 19:24:05 +0200

    Dedupe react packages to avoid bugs + Ignore JSON files on EsLint to avoid false errors + Add image input handling (WIP)

--- src/components/DefaultImageInput.tsx
+++ src/components/DefaultImageInput.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ImageInput } from "../types/ImageInput";
 
 // TODO add proptypes
 // eslint-disable-next-line react/prop-types
 const DefaultImageInput: ImageInput = ({ width }) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
   return (
     <div style={{ flexBasis: `${width}%`, border: "1px dotted blue" }}>
-      <p style={{ textAlign: "center" }}>- Image -</p>
+      <input type="file" ref={inputRef} />
+      <div>
+        <img src="" alt="Image" />
+        <span>+</span>
+      </div>
     </div>
   );
 };
