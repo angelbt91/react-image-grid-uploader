53c2f6e4514c98a61c797ea83bac0b36b1abd171
Angel Blanco <angelbt91@gmail.com>
2023-06-20 21:35:34 +0200

    Add onChange handling + Added fluid and fixed modes + Added way to override default styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,91 @@
-import React from "react";
-import DefaultImageInput from "./components/DefaultImageInput";
-import { ImageInput } from "./types/ImageInput";
+import React, { useRef } from "react";
 
-interface Props {
-  onChange?: () => void;
+type onChange = (imageAsDataURL: string, position?: number) => void;
+
+interface ImageGridUploaderProps {
   containerStyles?: React.CSSProperties;
-  ImageInput?: ImageInput;
+  gridStyles?: React.CSSProperties;
+  cellStyles?: React.CSSProperties;
   images?: number;
   columns?: number;
+  mode?: "fluid" | "fixed";
+  cellMinWidth?: string;
+  onChange?: onChange;
 }
 
+interface ImageInputProps {
+  position: number;
+  cellStyles?: React.CSSProperties;
+  onChange: onChange;
+}
+
+const ImageInput = ({ position, cellStyles, onChange }: ImageInputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = React.useState<string>();
+  const defaultCellStyles: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1px dotted blue",
+    height: "150px", // TODO try to make this dynamic based on widdth?
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const { result } = reader;
+      if (typeof result !== "string") return;
+      setImage(result);
+      onChange(result, position);
+    };
+    reader.readAsDataURL(file); // TODO allow to format as other types
+  };
+
+  return (
+    <div style={{ ...defaultCellStyles, ...cellStyles }}>
+      <input type="file" ref={inputRef} style={{ display: "none" }} onChange={handleImageChange} />
+      {image && (
+        <img
+          src={image}
+          alt={`Image ${position + 1}`}
+          style={{ maxHeight: "100%", maxWidth: "100%" }}
+        />
+      )}
+      {!image && <span onClick={() => inputRef.current && inputRef.current.click()}>+</span>}
+    </div>
+  );
+};
+
+// TODO add proptypes
+// eslint-disable-next-line react/prop-types
 const ImageGridUploader = ({
   containerStyles = {},
-  ImageInput = DefaultImageInput,
-  images = 1,
-  columns = 6,
-}: Props): JSX.Element => {
-  const cellWidth = 100 / (columns + 1);
-  const emptyCells = Math.ceil(images / columns) * columns - images;
+  gridStyles = {},
+  cellStyles = {},
+  images = 5,
+  columns = 3,
+  mode = "fixed",
+  cellMinWidth = "150px",
+  onChange = (image, position) => console.log({ position, image }),
+}: ImageGridUploaderProps): JSX.Element => {
+  // TODO add a way to read the values of all the cells from parent component
+  const defaultGridStyles: React.CSSProperties = {
+    display: "grid",
+    gridTemplateColumns: `repeat(
+    ${mode === "fixed" ? columns : "auto-fit"},
+    ${mode === "fixed" ? "1fr" : "minmax(" + cellMinWidth + ", 1fr)"}
+    )`,
+    gap: "15px",
+  };
 
   return (
     <div style={containerStyles}>
-      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
+      <div style={{ ...defaultGridStyles, ...gridStyles }}>
         {Array.from("_".repeat(images)).map((_, index) => (
-          <ImageInput key={index} width={cellWidth} />
-        ))}
-        {Array.from("_".repeat(emptyCells)).map((_, index) => (
-          <div key={index} style={{ flexBasis: `${cellWidth}%`, border: "1px dotted blue" }}></div>
+          <ImageInput key={index} position={index} cellStyles={cellStyles} onChange={onChange} />
         ))}
       </div>
     </div>
--- src/types/ImageInput.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-interface ImageInputProps {
-  width: number;
-}
-
-export type ImageInput = React.ElementType<ImageInputProps>;
